3e2b74b0f12d99c876de4f3d422f25b94ab501fd
juan ruiz <juandiegoruiztobon817@gmail.com>
2024-05-21 19:23:04 -0500

    actualizacion

--- view/inicio.php
+++ view/inicio.php
@@ -22,7 +22,7 @@
   <aside class="main-sidebar sidebar-dark-primary elevation-4">
     <!-- Brand Logo -->
     <a href="../../index.html" class="brand-link">
-
+  
       <span class="brand-text font-weight-light">AdminLTE 3</span>
     </a>
 
@@ -47,7 +47,7 @@
               with font-awesome or any other icon font library -->
           
           <li class="nav-item">
-            <a href="../widgets.html" class="nav-link">
+            <a href="/widgets.php" class="nav-link">
               <i class="nav-icon fas fa-th"></i>
               <p>
                 Widgets
